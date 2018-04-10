5499c7c1e40052df6e8b80c894ae60ae8b369b37
Michael <onvergetelijkontbijt@icloud.com>
2018-04-10 11:10:06 +0200

    started standardizing master answers by means through replace method and regexs

--- part_1.py
+++ part_1.py
@@ -2,8 +2,9 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import re
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
 
 
 # import csv dataset as pandas dataframe
@@ -24,7 +25,7 @@ odi_df = odi_df.iloc[1:]
 #Now, let's print only the column headers
 print(odi_df.columns)
 
-#how does the timestamp look like? 
+#how does the timestamp look like?
 print(odi_df.iloc[:, 0])
 #as it seems to be standardized (probably handled by the polling platform) we'll leave it that way
 
@@ -33,4 +34,22 @@ print(odi_df.columns[1])
 # what is the frequency of such programs?
 odi_df.iloc[:, 1].value_counts().sort_index()
 
-#TO BE CONTINUED...
+#FOLLOWING CODE IS IMPRECISE AND NEEDS FIX
+
+# Here we find different ways of saying the same thing
+# Let's make them uniform.
+# starting from the AI master
+ai_regexs = [".*A.*I.*", "Artificial.*", "Ai", "^.*premaster", r"A.I."]
+ai_replaced = odi_df.iloc[:,1].replace(to_replace=ai_regexs, value="A.I.", regex=True)
+ai_replaced.value_counts().sort_index()
+
+# computational science
+cs_regexs = ["Com.*ience"]
+cs_ai_replaced = ai_replaced.replace(to_replace=cs_regexs, value="C.S.", regex=True)
+cs_ai_replaced.value_counts().sort_index()
+
+# Bioinformatics
+bi_regexs = ["\w*ioinformatics\w*"]
+bi_cs_ai_replaced = cs_ai_replaced.replace(to_replace=bi_regexs, value="Bioinformatics",
+regex=True)
+bi_cs_ai_replaced.value_counts().sort_index()
